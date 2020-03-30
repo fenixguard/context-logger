7c90f8a46b5d8b352c1981fa618f706b94b9e50f
Sergey Volkov <fenixguard1993@gmail.com>
2020-03-30 20:36:07 +0300

    fix context manager

--- context_manager.py
+++ context_manager.py
@@ -1,5 +1,3 @@
-# Генерация списка из 50 млн элементов и применение к нему фукнции set
-
 from contextlib import contextmanager
 from random import randint
 from datetime import datetime
@@ -9,12 +7,17 @@ from datetime import datetime
 def time_logger():
     date_start = datetime.now()
     print(f"Время начала работы: {date_start}")
-    yield # тут будет подставлен блок из конструции with
+    yield  # тут будет подставлен блок из конструции with
     date_end = datetime.now()
     print(f"Время окончания работы: {date_end}")
     print(f"Время работы: {date_end - date_start}")
+    print(f"Уникальные значения: {unical_id_list}")
 
 
 if __name__ == '__main__':
     with time_logger():
-        nums = set([randint(1, 100) for i in range(50_000_000)])
+        ids = {'user1': [randint(1, 100) for i in range(10_000_000)],
+               'user2': [randint(1, 100) for i in range(10_000_000)],
+               'user3': [randint(1, 100) for i in range(10_000_000)]}
+
+        unical_id_list = list(set(sum(ids.values(), [])))
